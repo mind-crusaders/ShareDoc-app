43071c9d154211210a145885eb32bfde2022e8bb
orison09 <orison09@gmail.com>
2018-06-25 21:38:05 +0800

    upload

--- controllers/documents.rb
+++ controllers/documents.rb
@@ -6,7 +6,7 @@ module Edocument
   # Web controller for Edocument API
   class App < Roda
     route('documents') do |routing|
-      routing.on do
+      routing.on 'documents' do
         routing.is do
           routing.get do
             if @current_user.logged_in?
@@ -18,16 +18,15 @@ module Edocument
             end
           end
         end
-
-        routing.is 'add' do
-          routing.get do
-            if @current_user.logged_in?
-              view :document_add, locals: {
-                current_user: @current_user
-              }
-            else
-              routing.redirect '/auth/login'
+        
+        routing.on 'add' do
+          routing.post do
+            document = Form::NewDocument.call(routing.params)
+            if profile.failure?
+              flash[:error] = Form.message_values(document)
+              routing.redirect "/account/#{@current_user.username}"
             end
+            AddDocument.new(App.config).call(@current_user,document)
           end
         end
 
