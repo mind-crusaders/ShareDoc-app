70b173d9c6b0447a7941ddd113556adc4eb95232
orison09 <orison09@gmail.com>
2018-06-25 21:50:20 +0800

    Upload

--- controllers/documents.rb
+++ controllers/documents.rb
@@ -33,7 +33,7 @@ module Edocument
           
           routing.post do
             document = Form::NewDocument.call(routing.params)
-            if profile.failure?
+            if document.failure?
               flash[:error] = Form.message_values(document)
               routing.redirect "/account/#{@current_user.username}"
             end
